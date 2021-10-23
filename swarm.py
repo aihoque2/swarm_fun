a0bd083058e87b4ff9de9399987abfcfeed27b21
aihoque2 <aihoque2@illinois.edu>
2021-10-23 17:03:17 -0500

    called an instance of PSO, but I need to learn math behind it better

--- swarm.py
+++ swarm.py
@@ -3,7 +3,17 @@ swarm.py
 some file for swarm intelligence
 
 """
+import pyswarms as ps
+from pyswarms.utils.functions import single_obj as fx
 from pyswarms import Swarm
+
 
 def gather_swarm():
     pass
+
+
+options = {'c1': 0.5, 'c2': 0.3, 'w':0.9}
+# Call instance of PSO
+optimizer = ps.single.GlobalBestPSO(n_particles=10, dimensions=2, options=options)
+# Perform optimization
+best_cost, best_pos = optimizer.optimize(fx.sphere, iters=100)
